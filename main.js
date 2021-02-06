d618d6e11d86ef985abf0ce68f2c23ed771c29df
sankhanilsen <76916705+sankhanilsen@users.noreply.github.com>
2021-02-06 13:49:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var image=
+   ["download.png",
+   "brothers-clipart-student-16.jpg",  
+   "unnamed.jpg",
+
+];
+var i=0;
+function move(){
+document.getElementById("monkey").src=image[i];
+i++;
+}
